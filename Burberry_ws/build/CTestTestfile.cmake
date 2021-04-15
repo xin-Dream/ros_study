a7eb4322a3fc6e994f57f454ccec1bed8452be7d
xin-dream <1812752073@qq.com>
2021-04-15 20:47:32 +0800

    SLAM test(delta has some errors)

--- Burberry_ws/build/CTestTestfile.cmake
+++ Burberry_ws/build/CTestTestfile.cmake
@@ -5,4 +5,9 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("ros_arduino_bridge/ros_arduino_bridge")
+subdirs("ros_arduino_bridge/ros_arduino_firmware")
+subdirs("ros_arduino_bridge/ros_arduino_python")
+subdirs("ros_arduino_bridge/ros_arduino_msgs")
 subdirs("delta_III_ros")
+subdirs("jetsonbot_description")
