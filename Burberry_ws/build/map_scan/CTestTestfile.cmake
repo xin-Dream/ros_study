4fe22773cc3cfe10017e172ebacf86c9bcecf0dd
xin-Dream <1812752073@qq.com>
2021-07-22 17:32:36 +0800

    sss

---
+++ Burberry_ws/build/map_scan/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dream/ros_study/Burberry_ws/src/map_scan
+# Build directory: /home/dream/ros_study/Burberry_ws/build/map_scan
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
